e46c97c84c87c668c3eefef46171940fd6e52adf
Elpis <uwahope007@gmail.com>
2018-07-28 17:22:50 +0100

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ if ( $text == "" ) {
 	 $response  = "CON Enter UID \n";
 	 // $response .= "1. Register Child \n";
 	 // $response .= "2. Administer Vaccine";
-
+   $uid= $text;
 	
 }else if ( !$text == "" ) {
   // Business logic for first level response
@@ -17,25 +17,26 @@ if ( $text == "" ) {
   $response .= "1. Register Child \n";
   $response .= "2. Administer Vaccine";
   
-}else if($text == "2") {
+}else if($text == "12345*1") {
  
   // Business logic for first level response
   // This is a terminal request. Note how we start the response with END
-  $response = "END Your phone number is $phoneNumber";
+  $response = "CON Enter name";
  
-}else if($text == "1*1") {
+}else if($text == "12345*1*henry") {
  
   // This is a second level response where the user selected 1 in the first instance
-  $accountNumber  = "ACC1001";
+  $date  = "12/12/16";
   // This is a terminal request. Note how we start the response with END
-  $response = "END Your account number is $accountNumber";
+  //$response = "END Your account number is $accountNumber";
+  $response = "CON Enter DOB DD/MM/YY";
  
-}else if ( $text == "1*2" ) {
+}else if ( $text == "12345*1*henry*$date" ) {
   
 	 // This is a second level response where the user selected 1 in the first instance
 	 $balance  = "NGN 10,000";
 	 // This is a terminal request. Note how we start the response with END
-	 $response = "END Your balance is $balance";
+	 $response = "CON Number in the family";
 }
 // Print the response onto the page so that our gateway can read it
 header('Content-type: text/plain');
