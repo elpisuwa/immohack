964f6f0b2d76175cc57b2d9289755c55698796da
Elpis <uwahope007@gmail.com>
2018-07-28 18:13:17 +0100

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,11 @@ $phoneNumber = $_POST["phoneNumber"];
 $text        = $_POST["text"];
 $uid = "12345";
 $name = "henry";
+$date  = "12/12/16";
+$no = 2;
+$phone = "08080808080";
+$mom = "ada";
+$emergency = "07070707070";
 if ( $text == "" ) {
 	 // This is the first request. Note how we start the response with CON
 	 $response  = "CON Enter UID \n";
@@ -29,7 +34,7 @@ if ( $text == "" ) {
 }else if($text == "$uid*1*$name") {
  
   // This is a second level response where the user selected 1 in the first instance
-  $date  = "12/12/16";
+  
   // This is a terminal request. Note how we start the response with END
   //$response = "END Your account number is $accountNumber";
   $response = "CON Enter DOB DD/MM/YY";
@@ -41,6 +46,37 @@ if ( $text == "" ) {
 	 // This is a terminal request. Note how we start the response with END
 	 $response = "CON Number in the family";
 }
+}else if ( $text == "12345*1*henry*$date*$no" ) {
+  
+	 // This is a second level response where the user selected 1 in the first instance
+	 $balance  = "NGN 10,000";
+	 // This is a terminal request. Note how we start the response with END
+	 $response = "CON Enter Parents Phone No";
+}
+else if ( $text == "12345*1*henry*$date*$no*phone" ) {
+  
+	 // This is a second level response where the user selected 1 in the first instance
+	 $balance  = "NGN 10,000";
+	 // This is a terminal request. Note how we start the response with END
+	 $response = "CON Name of Mother";
+}
+else if ( $text == "12345*1*henry*$date*$no*phone*$mom" ) {
+  
+	 // This is a second level response where the user selected 1 in the first instance
+	 $balance  = "NGN 10,000";
+	 // This is a terminal request. Note how we start the response with END
+	 $response = "CON Enter Emergency No";
+}
+else if ( $text == "12345*1*henry*$date*$no*phone*$mom*$emergency" ) {
+  
+	 // This is a second level response where the user selected 1 in the first instance
+	 $balance  = "NGN 10,000";
+	 // This is a terminal request. Note how we start the response with END
+// 	 $response = "CON Enter Emergency No";
+	$response = "END The record was saved succesfully";
+}
+
+
 // Print the response onto the page so that our gateway can read it
 header('Content-type: text/plain');
 echo $response;
